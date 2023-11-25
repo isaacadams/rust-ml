b098fbeb2fd6364ce99f28d38f3d8edde8b8851e
Isaac Adams <isaacdadams@pm.me>
2023-11-25 15:11:28 -0500

    refactoring using enums

--- src/activations.rs
+++ src/activations.rs
@@ -1,27 +1,33 @@
 use std::f64::consts::E;
 
-#[derive(Clone)]
-pub struct Activation<'a> {
-	pub function: &'a dyn Fn(f64) -> f64,
-	pub derivative: &'a dyn Fn(f64) -> f64,
+pub enum Activation {
+	IDENTITY,
+	SIGMOID,
+	TANH,
+	RELU,
 }
 
-pub const IDENTITY: Activation = Activation {
-	function: &|x| x,
-	derivative: &|_| 1.0,
-};
-
-pub const SIGMOID: Activation = Activation {
-	function: &|x| 1.0 / (1.0 + E.powf(-x)),
-	derivative: &|x| x * (1.0 - x),
-};
-
-pub const TANH: Activation = Activation {
-	function: &|x| x.tanh(),
-	derivative: &|x| 1.0 - (x.powi(2)),
-};
-
-pub const RELU: Activation = Activation {
-	function: &|x| x.max(0.0),
-	derivative: &|x| if x > 0.0 { 1.0 } else { 0.0 },
-};
+impl Activation {
+	pub fn function(&self, x: f64) -> f64 {
+		match &self {
+			Activation::IDENTITY => x,
+			Activation::SIGMOID => 1.0 / (1.0 + E.powf(-x)),
+			Activation::TANH => x.tanh(),
+			Activation::RELU => x.max(0.0),
+		}
+	}
+	pub fn derivative(&self, x: f64) -> f64 {
+		match &self {
+			Activation::IDENTITY => 1.0,
+			Activation::SIGMOID => x * (1.0 - x),
+			Activation::TANH => 1.0 - (x.powi(2)),
+			Activation::RELU => {
+				if x > 0.0 {
+					1.0
+				} else {
+					0.0
+				}
+			}
+		}
+	}
+}
